8a8a8747cde09f4246c66e3d27bb9db0061ec301
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 16:02:42 -0300

    update heigth of form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -18,7 +18,8 @@ export default Form;
 
 const StyledForm = styled.div`
   width: 700px;
-  /* height: 606px; */
+  height: 600px;
+  /* min-height: 500px; */
   margin: 0 auto;
   box-shadow: 0px 5px 16px rgba(8, 15, 52, 0.06);
   border-radius: 34px;
