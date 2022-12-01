bcb82fe61f7cbc6e5534d0d05e83ce1c93a5e93e
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 16:23:57 -0300

    create step two page

--- src/pages/StepTwo.jsx
+++ src/pages/StepTwo.jsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import Info from '../components/Info';
 import pxToRem from '../helpers/pxToRem';
 
 const StepTwo = () => {
-  return <div>step two</div>;
+  return (
+    <div>
+      <Info
+        title="Our services"
+        text="Please select which service you are interested in."
+      />
+    </div>
+  );
 };
 
 export default StepTwo;
