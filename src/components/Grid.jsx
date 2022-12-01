f168d5e1cc553d02312514b0e2c5b71b924b1af5
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 16:18:21 -0300

    create grid component

---
+++ src/components/Grid.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Grid = ({ children }) => {
+  return <StyledGrid>{children}</StyledGrid>;
+};
+
+export default Grid;
+
+const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
+`;
