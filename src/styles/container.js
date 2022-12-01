82582db7ce8ce2f5e7d58498c5d0ee37c51d7e44
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 16:01:42 -0300

    update container witdh

--- src/styles/container.js
+++ src/styles/container.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width: 1140px;
+  max-width: 700px;
   width: 100%;
   margin: 0 auto;
+  position: relative;
 `;
 
 export default Container;
