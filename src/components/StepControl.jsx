1e39493eafdc7792cee58e818800ef26f1260f55
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 21:49:16 -0300

    create step control component

---
+++ src/components/StepControl.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StepControl = ({ children }) => {
+  return <StyledStepControl>{children}</StyledStepControl>;
+};
+
+export default StepControl;
+
+const StyledStepControl = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  bottom: -100px;
+  left: -50px;
+  width: 700px;
+  padding-bottom: 50px;
+`;
