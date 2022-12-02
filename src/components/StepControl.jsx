c4d02e5b6bf2b57b7854d53fe39647f1c19526ba
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-01 21:55:58 -0300

    update style of step control component

--- src/components/StepControl.jsx
+++ src/components/StepControl.jsx
@@ -11,6 +11,7 @@ const StyledStepControl = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  flex-direction: row-reverse;
   position: relative;
   bottom: -100px;
   left: -50px;
