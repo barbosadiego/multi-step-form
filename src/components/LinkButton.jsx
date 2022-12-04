1d314138b5c66621710663338f376d0390a88548
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-04 08:42:11 -0300

    fix filled prop in link button component

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-const LinkButton = ({ children, fill = false, onClick }) => {
+const LinkButton = ({ children, filled = false, onClick }) => {
   return (
-    <StyledButton onClick={onClick} fill={fill}>
+    <StyledButton onClick={onClick} filled={filled}>
       {children}
     </StyledButton>
   );
@@ -20,11 +20,11 @@ const StyledButton = styled.button`
   align-items: center;
   justify-content: center;
   font-size: ${({ theme }) => theme.fontSize.title5};
-  font-weight: ${({ fill }) => (fill ? '700' : '400')};
-  background-color: ${({ fill }) => (fill ? '#4A3AFF' : '#ffffff')};
-  color: ${({ fill }) => (fill ? '#fff' : '#4A3AFF')};
+  font-weight: ${({ filled }) => (filled ? '700' : '400')};
+  background-color: ${({ filled }) => (filled ? '#4A3AFF' : '#ffffff')};
+  color: ${({ filled }) => (filled ? '#fff' : '#4A3AFF')};
   box-shadow: 0px 3px 12px rgba(74, 58, 255, 0.18);
-  border: ${({ fill }) => (fill ? 'none' : '1px solid #4A3AFF')};
+  border: ${({ filled }) => (filled ? 'none' : '1px solid #4A3AFF')};
   cursor: pointer;
 
   &:active {
